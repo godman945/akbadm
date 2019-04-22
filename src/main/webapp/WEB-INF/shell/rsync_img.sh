e0bacf9d8a35106f4f03ea8e8c2465b6b2c64e46
nicolee <nicolee@staff.pchome.com.tw>
2019-04-22 11:49:58 +0800

    update rsync_img.sh

--- src/main/webapp/WEB-INF/shell/rsync_img.sh
+++ src/main/webapp/WEB-INF/shell/rsync_img.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 date >> /home/webuser/akb/log/crontab/rsync_img.log
-rsync -av /home/webuser/akb/pfp/img/ kdpic1.mypchome.com.tw::img/
-rsync -av /home/webuser/akb/pfp/img/ kdpic2.mypchome.com.tw::img/
-rsync -av /home/webuser/akb/pfp/img/ kdpic3.mypchome.com.tw::img/
-rsync -av /home/webuser/akb/pfp/img/ kdpic4.mypchome.com.tw::img/
-rsync -av /home/webuser/akb/pfp/img/ kdpic5.mypchome.com.tw::img/
+rsync -av /home/webuser/akb/pfp/img/ 172.21.10.11::img/
+rsync -av /home/webuser/akb/pfp/img/ 172.21.10.12::img/
+rsync -av /home/webuser/akb/pfp/img/ 172.21.10.13::img/
+rsync -av /home/webuser/akb/pfp/img/ 172.21.10.14::img/
+rsync -av /home/webuser/akb/pfp/img/ 172.21.10.15::img/
