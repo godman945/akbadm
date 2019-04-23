650bccea78384e3ef452e4dcb24a079c46e34242
twkuo <twkuo@staff.pchome.com.tw>
2019-04-23 17:47:09 +0800

    update shell script

--- src/main/webapp/WEB-INF/shell/report.sh
+++ src/main/webapp/WEB-INF/shell/report.sh
@@ -1,2 +1,2 @@
 cd /home/webuser/akb/webapps/ROOT/WEB-INF/classes
-java -cp .:../lib/*:/opt/tomcat/lib/* com.pchome.akbadm.quartzs.ReportJob $*
+/opt/jvm/bin/java -cp .:../lib/*:/opt/tomcat/lib/* com.pchome.akbadm.quartzs.ReportJob $*
