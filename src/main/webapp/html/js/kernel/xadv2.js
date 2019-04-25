50ec5dce60990c8da6446c282f1cdc7f7c1d1a17
alexchen <alexchen@alexchen6264.online.com.tw>
2019-04-25 16:01:58 +0800

    update

--- src/main/webapp/html/js/kernel/xadv2.js
+++ src/main/webapp/html/js/kernel/xadv2.js
@@ -1,6 +1,6 @@
-
 //5
 window.onload = function() {
+	console.log("AAAA");
     var a = document.getElementsByTagName("style")[0];
     if (a != undefined && a != null) {
         var cssText = a.innerText;
