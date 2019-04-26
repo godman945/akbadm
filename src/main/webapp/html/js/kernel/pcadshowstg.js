a8608f7c57f2eb850f259a950b8aa7453ee88431
alexchen <alexchen@alexchen6264.online.com.tw>
2019-04-26 10:03:32 +0800

    update

--- src/main/webapp/html/js/kernel/pcadshowstg.js
+++ src/main/webapp/html/js/kernel/pcadshowstg.js
@@ -84,6 +84,7 @@ try{
 	for (var i = 0; i < document.getElementsByTagName("script").length; i++) {
 		if(document.getElementsByTagName("script")[i].src.includes('pcadshowstg.js')){
 			if("ADBACKUP" == document.getElementsByTagName("script")[i].previousElementSibling.previousElementSibling.previousElementSibling.tagName){
+				console.log(res)
 				docurl = encodeURIComponent(res);
 			}
 		}
