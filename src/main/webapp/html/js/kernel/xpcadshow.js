e689dda2f88f047b856dc0f6f54cb552372d5fc6
alexchen <alexchen@alexchen6264.online.com.tw>
2019-05-02 16:12:34 +0800

    update

--- src/main/webapp/html/js/kernel/xpcadshow.js
+++ src/main/webapp/html/js/kernel/xpcadshow.js
@@ -79,10 +79,11 @@ if (ptype == "S") {
 }
 try {
     for (var i = 0; i < document.getElementsByTagName("script").length; i++) {
-    	console.log(document.getElementsByTagName("script")[i].src);
-    	console.log(document.getElementsByTagName("script")[i].src.includes('xadv2.js'));
-    	console.log(document.getElementsByTagName("script")[i].previousElementSibling);
     	
+    	if(document.getElementsByTagName("script")[i].src.includes('xadv2.js')){
+    		console.log(document.getElementsByTagName("script")[i].previousElementSibling);
+    		console.log(document.getElementsByTagName("script")[i].previousElementSibling.tagName);
+    	}
     	
     	
     	
