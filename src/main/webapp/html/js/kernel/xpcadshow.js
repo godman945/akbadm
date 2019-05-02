cd3c08205a800b6deece67ae97bcb384147f58b4
alexchen <alexchen@alexchen6264.online.com.tw>
2019-05-02 16:08:18 +0800

    update

--- src/main/webapp/html/js/kernel/xpcadshow.js
+++ src/main/webapp/html/js/kernel/xpcadshow.js
@@ -79,12 +79,12 @@ if (ptype == "S") {
 }
 try {
     for (var i = 0; i < document.getElementsByTagName("script").length; i++) {
-    	console.log(document.getElementsByTagName("script")[i].src);
-    	console.log(document.getElementsByTagName("script")[i].src.includes('xpcadshow.js'));
+//    	console.log(document.getElementsByTagName("script")[i].src);
+//    	console.log(document.getElementsByTagName("script")[i].src.includes('xpcadshow.js'));
+//    	console.log(document.getElementById(""));
     	
-    	
-        if (document.getElementsByTagName("script")[i].src.includes('xpcadshow.js')) {
-            if ("ADBACKUP" == document.getElementsByTagName("script")[i].previousElementSibling.previousElementSibling.previousElementSibling.tagName) {
+        if (document.getElementsByTagName("script")[i].src.includes('xadv2.js')) {
+            if ("ADBACKUP" == document.getElementsByTagName("script")[i].previousElementSibling.previousElementSibling.tagName) {
                 docurl = "www.pchome.com.tw"
             }
         }
