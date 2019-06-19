ea89f095cd5f01694a60dd5131b3e0a96aa0d0af
alexchen <alexchen@alexchen6264.online.com.tw>
2019-06-19 10:04:27 +0800

    update

--- src/main/webapp/html/js/kernel/pcadshowstg.js
+++ src/main/webapp/html/js/kernel/pcadshowstg.js
@@ -89,9 +89,9 @@ try {
 var fig = "";
 var adurl = "";
 if (padssl == "false") {
-    adurl += "http://kdcl.pchome.com.tw/adshow2.html?pfbxCustomerInfoId=" + pad_customerId
+    adurl += "http://kwstg1.pchome.com.tw/adshow2.html?pfbxCustomerInfoId=" + pad_customerId
 } else {
-    adurl += "http://kdcl.pchome.com.tw/adshow2.html?pfbxCustomerInfoId=" + pad_customerId
+    adurl += "http://kwstg1.pchome.com.tw/adshow2.html?pfbxCustomerInfoId=" + pad_customerId
 }
 adurl += "&positionId=" + pid;
 adurl += "&padWidth=" + pad_width;
