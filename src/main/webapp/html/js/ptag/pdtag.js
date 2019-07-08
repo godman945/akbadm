c1253d251443492d3597300a8d6430469552dcf2
alexchen <alexchen@alexchen6264.online.com.tw>
2019-07-08 17:03:26 +0800

    pdtag test

--- src/main/webapp/html/js/ptag/pdtag.js
+++ src/main/webapp/html/js/ptag/pdtag.js
@@ -13,7 +13,10 @@ var prod_dis = "";
 var ec_stock_status = "";
 var pa_em_value = "";
 var pa_id = "";
-var paclUrl = location.protocol+"//pacl.pchome.com.tw/api/collect";
+var mark_id = "";
+var mark_layer1 = "";
+
+var paclUrl = location.protocol + "//pacl.pchome.com.tw/api/collect";
 (function() {
     var click = null;
     referer = encodeURIComponent(document.referrer);
@@ -23,7 +26,7 @@ var paclUrl = location.protocol+"//pacl.pchome.com.tw/api/collect";
     convert_click_flag = false;
     do_a(function() {
         doInitData();
-        doSendPaclData();
+        doSendPaclData()
     })
 })();
 var referer = "";
@@ -33,7 +36,7 @@ var webUrl = "";
 var fig = "";
 var convert_click_flag = null;
 var paclCodeObject = new Object();
-var ptagParamater = null
+var ptagParamater = null;
 var paclCodeObject = null;
 
 function do_a(callback) {
@@ -41,81 +44,86 @@ function do_a(callback) {
     paclCodeObject["data"] = {};
     ptagParamater = window.ptag.q;
     if (typeof callback === 'function') {
-        callback();
+        callback()
     }
 }
 
 function doInitData() {
-    var pa_id = "";
-    ptagParamater.forEach(function(element) {
-        var ptagType = element[0];
-        if ((element.length == 1 && element[0].paid == undefined) || (element.length == undefined)) {
-            return
-        }
-        if (element.length == 1 && element[0].paid != undefined) {
-            pa_id = ptagType.paid
-        }
-        if (element[0] == undefined) {
-            return
-        }
-        if (element[1] == 'convert' || element[1] == 'page_view' || element[1] == 'tracking') {
-            var eventType = element[1];
-            if (eventType == "page_view") {
-                page_view_opt1 = element[2].hasOwnProperty('op1') ? element[2].op1 : '';
-                page_view_opt2 = element[2].hasOwnProperty('op2') ? element[2].op2 : '';
-                pa_em_value = element[2].hasOwnProperty('pa_em_value') ? element[2].pa_em_value : '';
-                paclCodeObject.data['page_view'] = {
-                    'page_view_opt1': page_view_opt1,
-                    'page_view_opt2': page_view_opt2,
-                    'pa_em_value': pa_em_value,
-                    'pa_id': pa_id
-                }
-            }
-            if (eventType == "convert") {
-                convert_id = element[2].hasOwnProperty('convert_id') ? element[2].convert_id : '';
-                convert_price = element[2].hasOwnProperty('convert_price') ? element[2].convert_price : '';
-                convert_opt1 = element[2].hasOwnProperty('op1') ? element[2].op1 : '';
-                convert_opt2 = element[2].hasOwnProperty('op2') ? element[2].op2 : '';
-                pa_em_value = element[2].hasOwnProperty('pa_em_value') ? element[2].pa_em_value : '';
-                convert_click_flag = element[3] === 'click' ? true : false;
-                paclCodeObject.data['convert_' + convert_id] = {
-                    'convert_id': convert_id,
-                    'convert_price': convert_price,
-                    'convert_opt1': convert_opt1,
-                    'convert_opt2': convert_opt2,
-                    'pa_em_value': pa_em_value,
-                    'pa_id': pa_id,
-                    'convert_click_flag': convert_click_flag
-                }
-            }
-            if (eventType == "tracking") {
-                tracking_id = element[2].hasOwnProperty('tracking_id') ? element[2].tracking_id : '';
-                prod_id = element[2].hasOwnProperty('prod_id') ? element[2].prod_id : '';
-                prod_price = element[2].hasOwnProperty('prod_price') ? element[2].prod_price : '';
-                prod_dis = element[2].hasOwnProperty('prod_dis') ? element[2].prod_dis : '';
-                tracking_opt1 = element[2].hasOwnProperty('op1') ? element[2].op1 : '';
-                tracking_opt2 = element[2].hasOwnProperty('op2') ? element[2].op2 : '';
-                ec_stock_status = element[2].hasOwnProperty('ec_stock_status') ? element[2].ec_stock_status : '';
-                pa_em_value = element[2].hasOwnProperty('pa_em_value') ? element[2].pa_em_value : '';
-                paclCodeObject.data['tracking_' + tracking_id] = {
-                    'tracking_id': tracking_id,
-                    'prod_id': prod_id,
-                    'prod_price': prod_price,
-                    'prod_dis': prod_dis,
-                    'tracking_opt1': tracking_opt1,
-                    'tracking_opt2': tracking_opt2,
-                    'ec_stock_status': ec_stock_status,
-                    'pa_em_value': pa_em_value,
-                    'pa_id': pa_id
-                }
-            }
-        }
-    })
+	screen_x = screen.availWidth;
+    screen_y = screen.availHeight;
+	
+	var pa_id = "";
+	for ( var obj in ptagParamater) {
+		var element = ptagParamater[obj];
+		if(element[0].paid != undefined && element[0].paid != ''){
+			pa_id = element[0].paid;
+		}else if(element[0].event != undefined && element[0].event != ''){
+			var eventObj = element[0].event;
+			var eventType = element[0].event.type;
+			
+			if(eventType == 'convert'){
+				convert_id = eventObj.hasOwnProperty('convert_id') ? eventObj.convert_id : '';
+				convert_price = eventObj.hasOwnProperty('convert_price') ? eventObj.convert_price : '';
+				convert_opt1 = eventObj.hasOwnProperty('op1') ? eventObj.op1 : '';
+				convert_opt2 = eventObj.hasOwnProperty('op2') ? eventObj.op2 : '';
+				pa_em_value = eventObj.hasOwnProperty('pa_em_value') ? eventObj.pa_em_value : '';
+				convert_click_flag = eventObj.hasOwnProperty('click') ? eventObj.click : false;
+				paclCodeObject.data['convert_' + convert_id] = {
+			              'convert_id': convert_id,
+			              'convert_price': convert_price,
+			              'convert_opt1': convert_opt1,
+			              'convert_opt2': convert_opt2,
+			              'pa_em_value': pa_em_value,
+			              'pa_id': pa_id,
+			              'convert_click_flag': convert_click_flag
+				}
+			}else if(eventType == 'tracking'){
+				tracking_id = eventObj.hasOwnProperty('tracking_id') ? eventObj.tracking_id : '';
+				prod_id = eventObj.hasOwnProperty('prod_id') ? eventObj.prod_id : '';
+				prod_price = eventObj.hasOwnProperty('prod_price') ? eventObj.prod_price : '';
+				prod_dis = eventObj.hasOwnProperty('prod_dis') ? eventObj.prod_dis : '';
+				tracking_opt1 = eventObj.hasOwnProperty('op1') ? eventObj.op1 : '';
+				tracking_opt2 = eventObj.hasOwnProperty('op2') ? eventObj.op2 : '';
+				ec_stock_status = eventObj.hasOwnProperty('ec_stock_status') ? eventObj.ec_stock_status : '';
+				pa_em_value = eventObj.hasOwnProperty('pa_em_value') ? eventObj.pa_em_value : '';
+				paclCodeObject.data['tracking_' + tracking_id] = {
+						'tracking_id': tracking_id,
+						'prod_id': prod_id,
+						'prod_price': prod_price,
+						'prod_dis': prod_dis,
+						'tracking_opt1': tracking_opt1,
+						'tracking_opt2': tracking_opt2,
+						'ec_stock_status': ec_stock_status,
+						'pa_em_value': pa_em_value,
+						'pa_id': pa_id
+				}
+			}else if(eventType == 'page_view'){
+				page_view_opt1 = eventObj.hasOwnProperty('op1') ? eventObj.op1 : '';
+				page_view_opt2 = eventObj.hasOwnProperty('op2') ? eventObj.op2 : '';
+				pa_em_value = eventObj.hasOwnProperty('pa_em_value') ? eventObj.pa_em_value : '';
+				paclCodeObject.data['page_view'] = {
+                  'page_view_opt1': page_view_opt1,
+                  'page_view_opt2': page_view_opt2,
+                  'pa_em_value': pa_em_value,
+                  'pa_id': pa_id
+				}
+			}else if(eventType == 'mark'){
+				mark_id = eventObj.hasOwnProperty('mark_id') ? eventObj.mark_id : '';
+				mark_layer1 = eventObj.hasOwnProperty('mark_layer1') ? eventObj.mark_layer1 : '';
+				paclCodeObject.data['mark'] = {
+		                  'mark_id': mark_id,
+		                  'mark_layer1': mark_layer1,
+		                  'pa_id': pa_id
+				}
+			}
+		}
+	}
+	console.log(paclCodeObject);
 };
 
 function doSendPaclData() {
     for (var key in paclCodeObject.data) {
-        if (key.includes('convert')) {
+        if (key.indexOf('convert') >= 0) {
             convert_id = paclCodeObject.data[key].convert_id;
             convert_price = paclCodeObject.data[key].convert_price;
             convert_opt1 = paclCodeObject.data[key].convert_opt1;
@@ -126,7 +134,7 @@ function doSendPaclData() {
                 doConvert()
             }
         }
-        if (key.includes('tracking')) {
+        if (key.indexOf('tracking') >=0) {
             tracking_id = paclCodeObject.data[key].tracking_id;
             prod_id = paclCodeObject.data[key].prod_id;
             prod_price = paclCodeObject.data[key].prod_price;
@@ -138,7 +146,7 @@ function doSendPaclData() {
             pa_id = paclCodeObject.data[key].pa_id;
             doTracking()
         }
-        if (key.includes('page_view')) {
+        if (key.indexOf('page_view') >=0) {
             page_view_opt1 = paclCodeObject.data[key].page_view_opt1;
             page_view_opt2 = paclCodeObject.data[key].page_view_opt2;
             pa_em_value = paclCodeObject.data[key].pa_em_value;
@@ -150,17 +158,26 @@ function doSendPaclData() {
 
 function doConvert() {
     var img = new Image();
-    img.src = paclUrl + "?" + "fingerId=" + "" + "&paId=" + encodeURIComponent(pa_id) + "&screenX=" + encodeURIComponent(screen_x) + "&screenY=" + encodeURIComponent(screen_y) + "&paEmValue=" + encodeURIComponent(pa_em_value) + "&url=" + encodeURIComponent(webUrl) + "&paEvent=convert&convertId=" + encodeURIComponent(convert_id) + "&convertPrice=" + encodeURIComponent(convert_price) + "&op1=" + encodeURIComponent(convert_opt1) + "&op2=" + encodeURIComponent(convert_opt2) + "&referer=" + encodeURIComponent(referer)
+    img.src = paclUrl + "?" + "fingerId=" + "" + "&paId=" + encodeURIComponent(pa_id) + "&screenX=" + encodeURIComponent(screen_x) + "&screenY=" +
+        encodeURIComponent(screen_y) + "&paEmValue=" + encodeURIComponent(pa_em_value) + "&url=" + encodeURIComponent(webUrl) + "&paEvent=convert&convertId=" +
+        encodeURIComponent(convert_id) + "&convertPrice=" + encodeURIComponent(convert_price) + "&op1=" + encodeURIComponent(convert_opt1) + "&op2=" +
+        encodeURIComponent(convert_opt2) + "&referer=" + encodeURIComponent(referer)
 };
 
 function doPageView() {
     var img = new Image();
-    img.src = paclUrl + "?" + "fingerId=" + "" + "&paId=" + encodeURIComponent(pa_id) + "&screenX=" + encodeURIComponent(screen_x) + "&screenY=" + encodeURIComponent(screen_y) + "&paEmValue=" + encodeURIComponent(pa_em_value) + "&url=" + encodeURIComponent(webUrl) + "&paEvent=page_view&op1=" + encodeURIComponent(page_view_opt1) + "&op2=" + encodeURIComponent(page_view_opt2) + "&referer=" + encodeURIComponent(referer)
+    img.src = paclUrl + "?" + "fingerId=" + "" + "&paId=" + encodeURIComponent(pa_id) + "&screenX=" + encodeURIComponent(screen_x) + "&screenY=" +
+        encodeURIComponent(screen_y) + "&paEmValue=" + encodeURIComponent(pa_em_value) + "&url=" + encodeURIComponent(webUrl) + "&paEvent=page_view&op1=" +
+        encodeURIComponent(page_view_opt1) + "&op2=" + encodeURIComponent(page_view_opt2) + "&referer=" + encodeURIComponent(referer)
 };
 
 function doTracking() {
     var img = new Image();
-    img.src = paclUrl + "?" + "fingerId=" + "" + "&paId=" + encodeURIComponent(pa_id) + "&screenX=" + encodeURIComponent(screen_x) + "&screenY=" + encodeURIComponent(screen_y) + "&paEmValue=" + encodeURIComponent(pa_em_value) + "&url=" + encodeURIComponent(webUrl) + "&paEvent=tracking&trackingId=" + encodeURIComponent(tracking_id) + "&prodId=" + encodeURIComponent(prod_id) + "&prodPrice=" + encodeURIComponent(prod_price) + "&prodDis=" + encodeURIComponent(prod_dis) + "&op1=" + encodeURIComponent(tracking_opt1) + "&op2=" + encodeURIComponent(tracking_opt2) + "&referer=" + encodeURIComponent(referer) + "&ecStockStatus=" + encodeURIComponent(ec_stock_status)
+    img.src = paclUrl + "?" + "fingerId=" + "" + "&paId=" + encodeURIComponent(pa_id) + "&screenX=" + encodeURIComponent(screen_x) + "&screenY=" +
+        encodeURIComponent(screen_y) + "&paEmValue=" + encodeURIComponent(pa_em_value) + "&url=" + encodeURIComponent(webUrl) + "&paEvent=tracking&trackingId=" +
+        encodeURIComponent(tracking_id) + "&prodId=" + encodeURIComponent(prod_id) + "&prodPrice=" + encodeURIComponent(prod_price) + "&prodDis=" +
+        encodeURIComponent(prod_dis) + "&op1=" + encodeURIComponent(tracking_opt1) + "&op2=" + encodeURIComponent(tracking_opt2) + "&referer=" +
+        encodeURIComponent(referer) + "&ecStockStatus=" + encodeURIComponent(ec_stock_status)
 };
 
 function pchome_click(link_url, blank_flag) {
@@ -197,7 +214,7 @@ function pchome_click() {
         doInitData()
     });
     for (var key in paclCodeObject.data) {
-        if (key.includes('convert')) {
+        if (key.indexOf('convert') >= 0) {
             convert_id = paclCodeObject.data[key].convert_id;
             convert_price = paclCodeObject.data[key].convert_price;
             convert_opt1 = paclCodeObject.data[key].convert_opt1;
